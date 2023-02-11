c496b4866a463368819dd6bac424e052e59576df
Fly <535651833@qq.com>
2023-02-11 10:41:01 +0800

    [feat]从头到尾打印链表

---
+++ 剑指offer/5.从尾到头打印链表.js
@@ -0,0 +1,22 @@
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）
+ * 输入：head = [1,3,2]
+ * 输出：[2,3,1]
+ * 0 <= 链表长度 <= 10000
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+
+let reversePrint = function(head) {
+    if (!head) return []
+    let arr = []
+    while (head) {
+        arr.push(head.val)
+        head=head.next
+    }
+    return arr.reverse()
+};
+reversePrint([1,3,2])
