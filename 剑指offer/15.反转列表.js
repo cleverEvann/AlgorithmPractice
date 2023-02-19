539bf6442b1a07f044bc987e011a492ec38ea648
Evann <535651833@qq.com>
2023-02-19 22:19:15 +0800

    [feat]反转链表

---
+++ 剑指offer/15.反转列表.js
@@ -0,0 +1,28 @@
+/**
+ * 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null, curr = head
+  while (curr) {
+    const next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+  return prev
+};
