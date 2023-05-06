23c70e95c0f355d8c6d1822319f705be71396b27
Fly <535651833@qq.com>
2023-05-06 10:00:05 +0800

    [feat]检查是否是类的对象实例

---
+++ javascript题型/3.检查是否是类的对象实例.js
@@ -0,0 +1,26 @@
+/**
+ * 请你编写一个函数，检查给定的对象是否是给定类或超类的实例。
+ * 可以传递给函数的数据类型没有限制。
+ * 示例 1：
+ * 输入：func = () => checkIfInstance(new Date(), Date)
+ * 输出：true
+ * 解释：根据定义，Date 构造函数返回的对象是 Date 的一个实例。
+ */
+
+/**
+ * @param {any} object
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+var checkIfInstanceOf = function(obj, classFunction) {
+    if(obj === undefined || obj === null) return false;
+    while(obj!==null) {
+        if(obj.constructor === classFunction) return true;
+        obj = Object.getPrototypeOf(obj);
+    }
+    return false;
+};
+
+/**
+ * checkIfInstanceOf(new Date(), Date); // true
+ */
