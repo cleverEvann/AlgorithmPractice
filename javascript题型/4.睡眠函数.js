2ca75ce694ea9e530307e530e3279322f399ea5d
Fly <535651833@qq.com>
2023-05-08 09:39:34 +0800

    [feat]睡眠函数

---
+++ javascript题型/4.睡眠函数.js
@@ -0,0 +1,23 @@
+/**
+ * 请你编写一个异步函数，它接收一个正整数参数 millis ，并休眠这么多毫秒。要求此函数可以解析任何值
+ * 输入：millis = 100
+ * 输出：100
+ * 解释：
+ * 在 100ms 后此异步函数执行完时返回一个 Promise 对象
+ * let t = Date.now();
+ * sleep(100).then(() => {
+ * console.log(Date.now() - t); // 100
+ * });
+ */
+
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    await new Promise((s) => setTimeout(s,millis))
+}
+
+/** 
+* let t = Date.now()
+* sleep(100).then(() => console.log(Date.now() - t)) // 100
+*/
