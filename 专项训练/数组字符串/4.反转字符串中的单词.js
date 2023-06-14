18365ac319dcb6333004a9e9f518a43f0a13149e
Fly <535651833@qq.com>
2023-06-14 16:54:29 +0800

    [fix]

--- 专项训练/数组字符串/4.反转字符串中的单词.js
+++ 专项训练/数组字符串/4.反转字符串中的单词.js
@@ -13,4 +13,12 @@
  * 输入：s = "a good   example"
  * 输出："example good a"
  * 解释：如果两个单词间有多余的空格，反转后的字符串需要将单词间的空格减少到仅有一个。
- */  
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    return s.trim().split(/\s+/).reverse().join(' ');
+   };
