41027cba606c6becb4832702527e20f922da1432
Fly <535651833@qq.com>
2023-07-31 13:57:45 +0800

    [feat]找出两数组的不同

---
+++ 专项训练/哈希表/2.找出两数组的不同.js
@@ -0,0 +1,36 @@
+/**
+ * 给你两个下标从 0 开始的整数数组 nums1 和 nums2 ，请你返回一个长度为 2 的列表 answer ，其中：
+ * answer[0] 是 nums1 中所有 不 存在于 nums2 中的 不同 整数组成的列表。
+ * answer[1] 是 nums2 中所有 不 存在于 nums1 中的 不同 整数组成的列表。
+ * 注意：列表中的整数可以按 任意 顺序返回。
+ * 输入：nums1 = [1,2,3], nums2 = [2,4,6]
+ * 输出：[[1,3],[4,6]]
+ * 解释：
+ * 对于 nums1 ，nums1[1] = 2 出现在 nums2 中下标 0 处，然而 nums1[0] = 1 和 nums1[2] = 3 没有出现在 nums2 中。因此，answer[0] = [1,3]。
+ * 对于 nums2 ，nums2[0] = 2 出现在 nums1 中下标 1 处，然而 nums2[1] = 4 和 nums2[2] = 6 没有出现在 nums2 中。因此，answer[1] = [4,6]。
+ */
+
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+var findDifference = function(nums1, nums2) {
+    let set1 = new Set(nums1), set2 = new Set(nums2)
+    let ans = []
+    let arr1 = [], arr2 = []
+    for (let item of nums1) {
+        if (!set2.has(item)) {
+            arr1.push(item)
+        }
+    }
+    ans.push([...new Set(arr1)])
+    for (let item of nums2) {
+        if (!set1.has(item)) {
+            arr2.push(item)
+        }
+    }
+    ans.push([...new Set(arr2)])
+    return ans
+};
